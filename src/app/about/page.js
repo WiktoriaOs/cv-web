fabe2b227108f034dc0ef0bef1879f21f59781f0
Wiktoria <abc@example.com>
2024-03-13 12:18:59 +0100

    strona about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -17,47 +17,45 @@ export default () => {
       <p>Pisanie strony za pomocą NEXT.js</p>
     </div>
     <header className={classes.header}>
-   
-    <div className={classes.section}>
-      <Image src={logoImg} className={classes.img}/>    
-    </div>
-    <div className={classes.main}>
-      
-      <div className={classes.center}>
-        <h3>Jestem Wiktoria, Jestem Front-end Developerem</h3>
-        <p>Front-end Developer jest odpowiedzialny za implementację 
-          elementów wizualnych, które są widoczne dla użytkowników i 
-          z którymi wchodzą oni w interakcję w aplikacji lub stronie 
-          internetowej. Optymalizuje działanie projektów pod jak 
-          najkrótsze czasy ładowania, czy np. responsywność.
-        </p>
-        <hr/>
-        <table>
-          <tr>
-            <th>Name:</th>
-            <td>Wiktoria</td>
-          </tr>
-          <tr>
-            <th>Data urodzenia: </th>
-            <td>31 pażdziernik 2006</td>
-          </tr>
-          <tr>
-            <th>Adres:</th>
-            <td>Radom</td>
-          </tr>
-          <tr>
-            <th>Email:</th>
-            <td>wiktoria.osobka@wp.pl</td>
-          </tr>
-          <tr>
-            <th>Telefon:</th>
-            <td>+48 000 000 000</td>
-          </tr>
-        </table>
-        <br/>
-        <Button/>
+      <div className={classes.section}>
+        <Image src={logoImg} className={classes.img}/>    
       </div>
-    </div>
+      <div className={classes.main}>
+        <div className={classes.center}>
+          <h3>Jestem Wiktoria, Jestem Front-end Developerem</h3>
+          <p>Front-end Developer jest odpowiedzialny za implementację 
+            elementów wizualnych, które są widoczne dla użytkowników i 
+            z którymi wchodzą oni w interakcję w aplikacji lub stronie 
+            internetowej. Optymalizuje działanie projektów pod jak 
+            najkrótsze czasy ładowania, czy np. responsywność.
+          </p>
+          <hr/>
+          <table>
+            <tr>
+              <th>Name:</th>
+              <td>Wiktoria</td>
+            </tr>
+            <tr>
+              <th>Data urodzenia: </th>
+              <td>31 pażdziernik 2006</td>
+            </tr>
+            <tr>
+              <th>Adres:</th>
+              <td>Radom</td>
+            </tr>
+            <tr>
+              <th>Email:</th>
+              <td>wiktoria.osobka@wp.pl</td>
+            </tr>
+            <tr>
+              <th>Telefon:</th>
+              <td>+48 000 000 000</td>
+            </tr>
+          </table>
+          <br/>
+          <Button/>
+        </div>
+      </div>
     </header>
     </>
   );
