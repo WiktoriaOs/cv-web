0178ebc4cb2688ff54ead8117c7b06f095bc47e7
Wiktoria <abc@example.com>
2024-03-19 14:58:36 +0100

    test image

--- src/app/about/image.js
+++ src/app/about/image.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import logoImg from '@/app/about/images/1.jpg';
  
-export function Avatar({ id, alt }) {
+export default function Avatar({ id, alt }) {
   return <Image src={`/about/${id}.jpg`} alt={alt} width="64" height="64" />
 }
  
-export function AvatarOfMe() {
-  return <Avatar id="1" alt="A portrait of me" />
+export default function Avataro() {
+  return <Avatar id={logoImg} alt="A portrait of me" />
 }
